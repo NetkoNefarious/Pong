0b9b6b4f726456d85b6c3ca1fadc388a57d183d6
NetkoNefarious <jmagazin5@gmail.com>
2018-04-21 16:46:49 +0200

    You can now choose between singleplayer and multiplayer

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -1,12 +1,21 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour {
+    public static bool isSinglePlayer;
 
-    public void PlayGame()
+    public void SinglePlayerGame()
     {
+        isSinglePlayer = true;
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void MultiPlayerGame()
+    {
+        isSinglePlayer = false;
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
 
--- Assets/Scripts/MovePaddle.cs
+++ Assets/Scripts/MovePaddle.cs
@@ -4,20 +4,34 @@ using UnityEngine;
 
 public class MovePaddle : MonoBehaviour {
     [SerializeField] float speed = 30;
+    [SerializeField] string axis = "Vertical";
+
+    public void Start()
+    {
+        SingleOrMultiPlayer();
+    }
+
+    private void SingleOrMultiPlayer()
+    {
+        if (axis == "Vertical2")
+        {
+            if (MainMenu.isSinglePlayer)
+            {
+                gameObject.GetComponent<MovePaddle>().enabled = false;
+                gameObject.GetComponent<AIPaddle>().enabled = true;
+            }
+
+            else
+            {
+                gameObject.GetComponent<MovePaddle>().enabled = true;
+                gameObject.GetComponent<AIPaddle>().enabled = false;
+            }
+        }
+    }
 
     void FixedUpdate()
     {
-        float vertPress = Input.GetAxisRaw("Vertical");
+        float vertPress = Input.GetAxisRaw(axis);
         GetComponent<Rigidbody2D>().velocity = new Vector2(0, vertPress) * speed;
     }
-
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
 }
