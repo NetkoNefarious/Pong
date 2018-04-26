8f793f7b4cbb01b380c2f2b36852304fe49d7825
NetkoNefarious <jmagazin5@gmail.com>
2018-04-26 23:02:34 +0200

    Fixed the bug with duplicating music when going back to main menu.

--- Assets/Scripts/ContinueMusic.cs
+++ Assets/Scripts/ContinueMusic.cs
@@ -2,13 +2,26 @@
 
 public class ContinueMusic : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
+    // Flag
+    private static bool isMusicPlaying = false;
+
+    // Use this for initialization
+    void Start () {
+
+        // If the music is not playing
+        if (!isMusicPlaying)
+        {
+            isMusicPlaying = true; // Set to true to signify that the music is now playing
+        }
+
+        else
+        {
+            Destroy(gameObject); // Destroy the newly-created Music object
+        }
+    }
 	
 	// Update is called once per frame
 	void Update () {
-        DontDestroyOnLoad(this.gameObject);
+        DontDestroyOnLoad(gameObject);
 	}
 }
